45524793f2e645baed5b2c928fa2f822c0311569
ibrahimkuranglebih <mumtazsamadikun765@gmail.com>
2026-01-23 18:11:09 +0700

    update some schema

--- dbt/football_dbt/models/marts/fact_matches.sql
+++ dbt/football_dbt/models/marts/fact_matches.sql
@@ -1,18 +1,10 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='table'
+    )
+}}
 
-SELECT
-    id,
-    date,
-    competition_id,
-    season_id,
-    duration_status,
-    CASE
-        WHEN result = 'HOME_TEAM' THEN home_team
-        WHEN result = 'AWAY_TEAM' THEN away_team
-        WHEN result = 'DRAW' THEN 'DRAW'
-        ELSE NULL
-    END AS winner,
-    home_final_score,
-    away_final_score
-FROM {{ ref('dim_matches') }}
-WHERE status = 'FINISHED'
+select * from 
+{{ref('stg_matches')}}
+order by 
+id
